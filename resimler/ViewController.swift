f0ddc669dee59c6308b38eeadc522e5f448204b4
erdemoden <erdemoden5@gmail.com>
2021-05-08 18:36:04 +0300

    Initial Commit

---
+++ resimler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  resimler
+//
+//  Created by erdem öden on 8.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
