29216fe9f1ee41ece71a034a8d258c17532803b4
erdemoden <erdemoden5@gmail.com>
2021-05-09 00:03:48 +0300

    first commit

---
+++ resimler/cellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  cellTableViewCell.swift
+//  resimler
+//
+//  Created by erdem öden on 8.05.2021.
+//
+
+import UIKit
+
+class cellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
